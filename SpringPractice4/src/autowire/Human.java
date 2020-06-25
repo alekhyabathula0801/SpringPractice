1a4c4e0e106b4c41a66c3a797b6f271a540e7254
arun kumar <alekhya0801@gmail.com>
2020-06-25 23:10:10 +0530

    Refactored : autowire dependency by constructor

--- SpringPractice4/src/autowire/Human.java
+++ SpringPractice4/src/autowire/Human.java
@@ -3,6 +3,10 @@ package autowire;
 public class Human {
     private Heart heart;
 
+    public Human(Heart heart) {
+        this.heart = heart;
+    }
+
     public Heart getHeart() {
         return heart;
     }
